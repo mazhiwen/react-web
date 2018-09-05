3996d34bb249289a38eea526d2d88ca2f042520d
marjoven <81245995@qq.com>
2018-09-05 18:14:29 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    
+    return (
+      <Router>
+        <div className="App">
+          {/* <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+
+          </p> */}
+          <header>
+            <div>第二</div>
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/topics">Topics</Link>
+              用户
+            </div>
+            
+          </header>
+        </div>
+
+
+      </Router>
+    );
+  }
+}
+
+export default App;
