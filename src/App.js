c20a8216bb1c5c48abe74ab84d3a0ce22e172798
marjoven <81245995@qq.com>
2018-09-05 18:27:57 +0800

    less,css，extractt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';import './App.css';
+
 import {
   BrowserRouter as Router,
   Route,
