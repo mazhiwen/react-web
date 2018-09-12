64f1b4e216d97ad56a241106d0fe073c2e45479c
marjoven <81245995@qq.com>
2018-09-12 18:34:56 +0800

    首页

--- src/App.js
+++ src/App.js
@@ -1,48 +1,77 @@
 import React, { Component } from 'react';
 import logo from './images/logo.png';
-import './App.less';
+import './styles/index.less';
 import routes from './routes';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
-import {Paper,Menu,MenuItem} from '@material-ui/core';
+import {Paper} from '@material-ui/core';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import { light } from '@material-ui/core/styles/createPalette';
 
-
+const theme = createMuiTheme({
+  palette: {
+    type:'light',
+    primary: { 
+      main: '#1a1a1a', 
+      // light:'#909090',
+      // dark:'#515151'
+    }, 
+    secondary: { 
+      main: '#90caf9',
+      // light:'#a6d4fa',
+      // dark:'#648dae'
+    }, 
+    text:{
+      primary:'#757575'
+    }
+  },
+  overrides:{
+    MuiFormControl: {
+      root: {
+        // padding:'1rem',
+        // marginRight:15
+      },
+    }
+  }
+});
 
 class App extends Component {
   render() {
     
     return (
       <Router>
-        <div className="App">   
-          <Paper className="head">
-            <div className="head-inner">
-              <div className="head-left">
-                <img className="logo" src={logo} alt="logo"/>
-                <span>第二</span>
+        <MuiThemeProvider theme={theme}>
+          <div className="App">   
+            <Paper className="head">
+              <div className="head-inner">
+                <div className="head-left">
+                  <img className="logo" src={logo} alt="logo"/>
+                  <span>第二</span>
+                </div>
+                <div>
+                  <Link to={routes.game.list.path}>游戏</Link>
+                  <Link to="/about">动态</Link>
+                </div>
+                <div>
+                  <Link to="/topics">我</Link>
+                  <Link to="/topics">赞助商</Link>
+                </div>
               </div>
-              <div>
-                <Link to={routes.game.list.path}>游戏</Link>
-                <Link to="/about">动态</Link>
-              </div>
-              <div>
-                <Link to="/topics">我</Link>
-                <Link to="/topics">赞助商</Link>
-              </div>
+            </Paper>
+            <div className="content">
+              <Route path={routes.game.list.path} component={routes.game.list.component} />
+              <Route path={routes.game.details.path} component={routes.game.details.component} />
+
             </div>
-          </Paper>
-          <Paper className="content">
-            <Route path={routes.game.list.path} component={routes.game.list.component} />
-            <Route path={routes.game.details.path} component={routes.game.details.component} />
-
-          </Paper>
-          <footer>
-            footer
-          </footer>
-        </div>
-
+            <footer>
+              footer
+            </footer>
+          </div>
+        </MuiThemeProvider>
 
       </Router>
     );
--- src/views/game/list/index.js
+++ src/views/game/list/index.js
@@ -5,168 +5,189 @@ import {
 } from 'react-router-dom';
 import logo from '../../../images/logo.png';
 import routes from '../../../routes';
-import {TextField,Button,Chip,Select,MenuItem,FormControl,InputLabel,FormLabel,Checkbox,FormControlLabel,FormGroup} from '@material-ui/core';
+import {TextField,Button,Chip,Select,MenuItem,FormControl,InputLabel,Divider,Paper,FormControlLabel,FormGroup} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-
+const StyledSelect = withStyles({
+  root: {
+    width: 100,
+  }
+})(Select);
+const sortItemForm=(
+  <FormControl style={{
+    marginLeft:20
+  }}></FormControl>
+);
+const mainLabel=()=>(
+  <label className="main-label">11</label>
+);
 class componentInstance extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sortItemShow: false,
-      params:{
-        sortScore:''
+constructor(props) {
+  super(props);
+  this.state = {
+    sortItemShow: false,
+    params:{
+      sortScore:''
+    },
+    gameList:[
+      {
+        name:'name1',
+        score:'32131',
+        hot:'11'
       },
-      gameList:[
-        {
-          name:'name1',
-          score:'32131',
-          hot:'11'
-        },
-        {
-          name:'name2',
-          score:'',
-          hot:''
-        }
-      ],
-      releaseYearList:[
-        { 
-          value:'2018888',
-          label:2018
-        },
-        { 
-          value:'20189999',
-          label:2019
-        }
-      ],
-      age:''
-    };
-  }
-  handleChange=()=>e=>{
-    this.setState({
-      'sortItemShow':e.target.value
-    })
-  }
-  handleClick=(value)=>{
-    console.log(value);
-  }
-  handleGameItemClick = (id,event) => {
-    // console.log(`${routes.game.details.path}/${id}`);
-    this.props.history.push(`${routes.game.details.base}/${id}`);
+      {
+        name:'name2',
+        score:'',
+        hot:''
+      }
+    ],
+    releaseYearList:[
+      { 
+        value:'2018888',
+        label:2018
+      },
+      { 
+        value:'20189999',
+        label:2019
+      }
+    ],
+    age:''
   };
-  render() {
-    return (
-      <Router>
+}
+handleChange=()=>e=>{
+  this.setState({
+    'sortItemShow':e.target.value
+  })
+}
+handleClick=(value)=>{
+  console.log(value);
+}
+handleGameItemClick = (id,event) => {
+  // console.log(`${routes.game.details.path}/${id}`);
+  this.props.history.push(`${routes.game.details.base}/${id}`);
+};
+render() {
+  return (
+  <Router>
+  <div className="gamelist">
+    <Paper className="sl-paper">
+      <div className="search-box">
+        <TextField
+          id="name"
+          value={this.state.sortItemShow}
+          onChange={this.handleChange()}
+          margin="normal"
+        />
+        <Button className="search-btn" variant="outlined">
+          找一找
+        </Button>
+      </div>
+
+      <div className="filter-box">
+        <label className="main-label">发布年限</label>
         <div>
-
-          <TextField
-            id="name"
-            value={this.state.sortItemShow}
-            onChange={this.handleChange()}
-            margin="normal"
-          />
-          <Button variant="outlined">
-            找一找
-          </Button>
-
-          
-          <div className="filter-box">
-            <span>发布年限</span>
-            <div>
-              {
-                this.state.releaseYearList.map((value,key)=>
-                  <Chip key={key} variant="outlined" label={value.label} onClick={this.handleClick.bind(this,value.value)}/>
-                )
-              }
-            </div>
-          </div>
-          <div className="filter-box">
-            <span>题材</span>
-            <div>
-              {
-                this.state.releaseYearList.map((value,key)=>
-                  <Chip key={key} variant="outlined" label={value.label} onClick={this.handleClick.bind(this,value.value)}/>
-                )
-              }
-            </div>
-          </div>
-          <div className="filter-box">
-            <span>平台</span>
-            <div>
-              {
-                this.state.releaseYearList.map((value,key)=>
-                  <Chip key={key} variant="outlined" label={value.label} onClick={this.handleClick.bind(this,value.value)}/>
-                )
-              }
-            </div>
-          </div>
-
-          <div className="sort-wrap">
-            <span>排序</span>
-            <FormControl>
-              <InputLabel htmlFor="age-simple">评分</InputLabel>
-              <Select
-                value={this.state.age}
-                onChange={this.handleClick}
-                inputProps={{
-                  name: 'age',
-                  id: 'age-simple',
-                }}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>  
-            <FormControl>
-              <InputLabel htmlFor="age-simple">热度</InputLabel>
-              <Select
-                value={this.state.age}
-                onChange={this.handleClick}
-                inputProps={{
-                  name: 'age',
-                  id: 'age-simple',
-                }}
-              >
-                <MenuItem value={10}>评分从高到低</MenuItem>
-                <MenuItem value={20}>评分从高到低</MenuItem>
-              </Select>
-            </FormControl>  
-            <FormControl>
-              <InputLabel htmlFor="age-simple">发布年限</InputLabel>
-              <Select
-                value={this.state.age}
-                onChange={this.handleClick}
-                inputProps={{
-                  name: 'age',
-                  id: 'age-simple',
-                }}
-              >
-                <MenuItem value={10}>评分从高到低</MenuItem>
-                <MenuItem value={20}>评分从高到低</MenuItem>
-              </Select>
-            </FormControl> 
-
-          </div>
-
-          <div>
-            {
-              this.state.gameList.map((value,key)=>
-                <div key={key} onClick={this.handleGameItemClick.bind(this,key)} className="game-item-box">
-                  {value.name}
-                  <img src={logo} alt="logo"/>
-                  评分：{value.score}
-                  热度：{value.hot}
-                </div>
-              )
-            }
-          </div>
+          {
+            this.state.releaseYearList.map((value,key)=>
+              <Chip className="filter-value" key={key} variant="outlined" label={value.label} onClick={this.handleClick.bind(this,value.value)}/>
+            )
+          }
         </div>
-      </Router>
-    );
-  }
+      </div>
+      <div className="filter-box">
+        <label className="main-label">题材</label>
+        <div>
+          {
+            this.state.releaseYearList.map((value,key)=>
+              <Chip className="filter-value" key={key} variant="outlined" label={value.label} onClick={this.handleClick.bind(this,value.value)}/>
+            )
+          }
+        </div>
+      </div>
+      <div className="filter-box">
+        <label className="main-label">平台</label>
+        <div>
+          {
+            this.state.releaseYearList.map((value,key)=>
+              <Chip className="filter-value" key={key} variant="outlined" label={value.label} onClick={this.handleClick.bind(this,value.value)}/>
+            )
+          }
+        </div>
+      </div>
+    </Paper>
+    <Paper className="sl-paper">      
+      <div className="sort-wrap">
+        <label className="main-label">排序</label>
+        <FormControl style={{marginRight:15}}>
+          <InputLabel htmlFor="age-simple">评分</InputLabel>
+          <StyledSelect
+            value={this.state.age}
+            onChange={this.handleClick}
+            classes={{
+              root:{
+                height: 48
+              }
+            }}
+            inputProps={{
+              name: 'age',
+              id: 'age-simple',
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </StyledSelect>
+        </FormControl>  
+        <FormControl style={{marginRight:15}}>
+          <InputLabel htmlFor="age-simple">热度</InputLabel>
+          <StyledSelect
+            value={this.state.age}
+            onChange={this.handleClick}
+            inputProps={{
+              name: 'age',
+              id: 'age-simple',
+            }}
+          >
+            <MenuItem value={10}>评分从高到低</MenuItem>
+            <MenuItem value={20}>评分从高到低</MenuItem>
+          </StyledSelect>
+        </FormControl>  
+        <FormControl style={{marginRight:15}}>
+          <InputLabel htmlFor="age-simple">发布年限</InputLabel>
+          <StyledSelect
+            value={this.state.age}
+            onChange={this.handleClick}
+            inputProps={{
+              name: 'age',
+              id: 'age-simple',
+            }}
+          >
+            <MenuItem value={10}>评分从高到低</MenuItem>
+            <MenuItem value={20}>评分从高到低</MenuItem>
+          </StyledSelect>
+        </FormControl> 
+
+      </div>
+
+      <div>
+        {
+          this.state.gameList.map((value,key)=>
+            <div key={key} onClick={this.handleGameItemClick.bind(this,key)} className="game-item-box">
+              {value.name}
+              <img src={logo} alt="logo"/>
+              评分：{value.score}
+              热度：{value.hot}
+            </div>
+          )
+        }
+      </div>
+    </Paper>  
+  </div>
+  </Router>
+  );
+}
 }
 
 export default withRouter(componentInstance);
