2b1995565081da61425f51bf6a4c3686665c9296
81245995@qq.com <81245995@qq.com>
2018-10-22 20:38:15 +0800

    up

--- src/views/game/details/index.js
+++ src/views/game/details/index.js
@@ -43,10 +43,18 @@ class componentInstance extends Component {
           <Paper>
             <Typography variant="title">评分</Typography>
             <table className="gird-table">
-              <caption></caption>
+              <caption>画面</caption>
               <tbody>
-                <tr><th>名称</th><td>名称(enname)</td><th>英文名称</th><td>enname</td><th>发行日期</th><td>enname</td></tr>
-                <tr><th>题材</th><td>中世纪</td><th>模式</th><td>arpg</td><th>平台</th><td>pc/web</td></tr>
+                <tr><th>模型细节可见</th><td>3</td><th>维度</th><td>23</td><th>热度</th><td>1222</td><th>光影真实</th><td>3</td></tr>
+                <tr><th>粒子效果</th><td>2</td><th>色调搭配</th><td>2</td><th>热度</th><td>1222</td><th>平台</th><td>pc/web</td></tr>
+                <tr><th>开发商</th><td>大叔大叔大叔大叔</td><th>评分</th><td>1231</td><th>热度</th><td>1222</td><th>热度</th><td>1222</td></tr>
+              </tbody>      
+            </table>
+            <table className="gird-table">
+              <caption>音效</caption>
+              <tbody>
+                <tr><th>模型细节可见</th><td>3</td><th>维度</th><td>23</td><th>光影真实</th><td>3</td></tr>
+                <tr><th>粒子效果</th><td>2</td><th>色调搭配</th><td>2</td><th>平台</th><td>pc/web</td></tr>
                 <tr><th>开发商</th><td>大叔大叔大叔大叔</td><th>评分</th><td>1231</td><th>热度</th><td>1222</td></tr>
               </tbody>      
             </table>
@@ -54,6 +62,10 @@ class componentInstance extends Component {
           <div>
             系统玩法
 
+
+            外交 
+
+            军事  制造  坦克，飞机
           </div>
           <div>
             界面操作
